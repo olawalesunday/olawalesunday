769853cc617c2c0a5c3ab503bd56cb5ec5d7b9c7
olawalesunday <oaiyeobasan@yahoo.com>
2018-03-02 01:37:32 +0100

    Add files via upload

---
+++ js/slide.js
@@ -0,0 +1,12 @@
+<script>
+		$(function () {
+		  $("#slider").responsiveSlides({
+			auto: true,
+			pager: false,
+			nav: true,
+			speed: 500,
+			maxwidth: 962,
+			namespace: "centered-btns"
+		  });
+		});
+	</script>
